ecb69dcc5fd4e3615472270daddcd22f422c5d17
vpxop111 <eaglesurvivor12@gmail.com>
2025-05-25 08:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ CORS(app,
          'https://pppp-351z.onrender.com',
          'https://infoui.vercel.app',
      'https://infoui-f2zp4fwwo-varuns-projects-859429fc.vercel.app',
+          'https://infoui-6fk6va0qk-varuns-projects-859429fc.vercel.app',
          'https://infoui.vercel.app/'
           
      ],
